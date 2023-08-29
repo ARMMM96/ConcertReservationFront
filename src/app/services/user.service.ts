7784fd22ab1e173f1a4d2a71d1e12d50f7ae1f9e
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-08-29 08:14:38 +0300

    User service created
    In this service register user method implemented

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+
+  registerUser(user: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/user/register`, user);
+  }
+
+}
