6d1bc165bc13acb61523896493d6f479f91dd53a
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2023-08-29 08:18:48 +0300

    AppRoutingModule HttpClientModule ReactiveFormsModule Imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +11,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 @NgModule({
@@ -17,11 +21,14 @@ import { FooterComponent } from './components/footer/footer.component';
     HeroSectionComponent,
     NavbarComponent,
     FooterComponent,
+    RegisterComponent,
   ],
   imports: [
     RouterModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
